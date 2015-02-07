9c7088c6ffbd35012fd4bc85094b0269f8bbe1f7
Kothai <kothaiaug1593@gmail.com>
2015-02-07 12:43:37 +0530

    First time commit

---
+++ simplejar/src/simple/hello.java
@@ -0,0 +1,19 @@
+package simple;
+
+public class hello
+{
+	
+	public static void displayhello()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Display Hello");
+	}
+
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+System.out.println("Display Hello");
+	}
+
+}
